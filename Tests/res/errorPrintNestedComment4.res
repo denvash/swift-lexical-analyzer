73763e85b5bc64d05af7d468eb13040454c2bc8e
denvash <justdenva@gmail.com>
2020-04-08 14:10:54 +0300

    [+] Add Tests

--- Tests/res/errorPrintNestedComment4.res
+++
@@ -1,1 +0,0 @@
-1 COMMENT 1
--- Tests/runTests.sh
+++ Tests/runTests.sh
@@ -14,34 +14,35 @@ app=./a.out
 testFolder=${workspace}/Tests
 inFolder=${testFolder}/in
 outFolder=${testFolder}/out
-resultFolder=${testFolder}/expected
+resultFolder=${testFolder}/res
 
 inExt=\.in
 outExt=\.out
-resExt=\.res
 
 totalTests=`ls -1q ${inFolder} | wc -l`
 
+# Test Run
 for filePath in ${inFolder}/*.in; do
 #    dos2unix ${filePath}
 
 # get the file name without extension
     fileName=`basename ${filePath} | cut -d . -f 1`
 
-# Debug
-    # echo ${filePath}
+# echo ${filePath}
     echo Runing Test ${fileName}
     ${app} < ${filePath} > ${outFolder}/${fileName}${outExt}
 done
 
-# for filePath in ${expectedFolder}/*.out; do
-# #    dos2unix ${filePath}
+# Diff
+for filePath in ${outFolder}/*.out; do
+#    dos2unix ${filePath}
 
-#     fileName=`echo ${filePath} | cut -d / -f 11 | cut -d . -f 1`
+# get the file name without extension
+    fileName=`basename ${filePath} | cut -d . -f 1`
 
-#     result=`diff ${filePath} ${outFolder}/${fileName}${outExt}`
-#     if [[ "${result}" != "" ]]; then
-#         echo ${result}
-#         echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Test ${fileName} FAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-#     fi
-# done
+    result=`diff ${filePath} ${resultFolder}/${fileName}${outExt}`
+    if [[ "${result}" != "" ]]; then
+        echo ${result}
+        echo "~~ Test ${fileName} FAILED"
+    fi
+done
